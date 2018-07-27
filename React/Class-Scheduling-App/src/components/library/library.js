c31095917d055c9188400a891e368120eacb9293
Jordan Hudgens <jordan@devcamp.com>
2018-07-27 13:47:52 -0600

    Implemented mapStateToProps and mapped over data to return the amount of course component instances required

--- React/Class-Scheduling-App/src/components/library/library.js
+++ React/Class-Scheduling-App/src/components/library/library.js
@@ -8,20 +8,29 @@ class Library extends Component {
   componentDidMount() {
     this.props.fetchCourses();
   }
+  renderCourses() {
+    const data = this.props.courses;
+
+    return data.map((object, index) => {
+      return <LibraryCourse key={index} />;
+    });
+  }
 
   render() {
     return (
       <div className="library">
         <h1 className="library__title">Course Library</h1>
-        <LibraryCourse />
-        <LibraryCourse />
-        <LibraryCourse />
+        {this.renderCourses()}
       </div>
     );
   }
 }
-
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
 export default connect(
-  null,
+  mapStateToProps,
   actions
 )(Library);
